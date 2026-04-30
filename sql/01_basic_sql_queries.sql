50d07da0458ecc43fd650eef957eec179bf7f5e6
meganlarson42 <malarson42@gmail.com>
2026-04-30 00:18:10 +0000

    Completed set-up, data download, data import, and SQL Assignment 1

--- sql/01_basic_sql_queries.sql
+++ sql/01_basic_sql_queries.sql
@@ -11,7 +11,8 @@
 
 -- TODO: Write your query below
 
-
+SELECT SUM(popn_asian) AS asian_pop
+FROM nyc_census_blocks;
 
 -- Exercise 2: What is the population of Manhattan?
 -- Expected output: one row with the total population of Manhattan
@@ -25,7 +26,9 @@
 
 -- TODO: Write your query below
 
-
+SELECT SUM(popn_total) AS manhattan_pop
+FROM nyc_census_blocks
+WHERE boroname = 'Manhattan';
 
 -- Exercise 3: For each borough, what percentage of the population is black?
 -- Expected output: one row per borough with borough name and black population percentage
@@ -45,7 +48,12 @@
 
 -- TODO: Write your query below
 
-
+SELECT
+    boroname,
+    100.0 * SUM(popn_black) / SUM(popn_total) AS percentage_black
+FROM nyc_census_blocks
+GROUP BY boroname
+ORDER BY boroname;
 
 
 -- Exercise 4: What is the average population per census block in each borough?
@@ -64,3 +72,10 @@
 -- Hint: Use ORDER BY avg_population DESC for readability
 
 -- TODO: Write your query below
+
+SELECT 
+  boroname, 
+  AVG(popn_total) AS avg_population
+FROM nyc_census_blocks
+GROUP BY boroname
+ORDER BY avg_population DESC;
