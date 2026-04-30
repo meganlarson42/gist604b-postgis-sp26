471124f2c36a9065a2c619b4f0d6a53112ef1544
meganlarson42 <malarson42@gmail.com>
2026-04-30 01:05:48 +0000

    Completed SQL Assignment 3

--- sql/03_spatial_relationships.sql
+++ sql/03_spatial_relationships.sql
@@ -10,7 +10,9 @@
 
 -- TODO: Write your query below
 
-
+SELECT ST_AsText(geom) AS queensboro
+FROM nyc_streets
+WHERE name = 'Queensboro Brg';
 
 
 -- Exercise 2: What neighborhood and borough is Queensboro Brg in?
@@ -25,7 +27,14 @@
 
 -- TODO: Write your query below
 
-
+SELECT 
+  name AS neighborhood, 
+  boroname AS borough
+FROM nyc_neighborhoods
+WHERE ST_Intersects(
+  geom, 
+  (SELECT geom FROM nyc_streets WHERE name = 'Queensboro Brg')
+);
 
 
 -- Exercise 3: What streets does Queensboro Brg intersect with?
@@ -43,7 +52,14 @@
 
 -- TODO: Write your query below
 
-
+SELECT DISTINCT name
+FROM nyc_streets
+WHERE ST_Intersects(
+  geom, 
+  (SELECT geom FROM nyc_streets WHERE name = 'Queensboro Brg')
+)
+AND name != 'Queensboro Brg'
+AND name IS NOT NULL;
 
 
 -- Exercise 4: Approximately how many people live within 50 meters of Queensboro Brg?
@@ -58,5 +74,11 @@
 
 -- TODO: Write your query below
 
-
+SELECT SUM(popn_total) AS total_population
+FROM nyc_census_blocks
+WHERE ST_DWithin(
+  geom, 
+  (SELECT geom FROM nyc_streets WHERE name = 'Queensboro Brg'),
+  50
+);
 
